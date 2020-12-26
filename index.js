d2f34dcc34d1527979516c20a758858a6ac857bd
Sanchit Anand <sanchitanand34@gmail.com>
2020-12-27 02:42:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 //importing
 import express from "express";
 import mongoose from "mongoose";
-import Messages from "./dbMessages.js";
 import Pusher from "pusher";
+import postRoutes from "./routes/posts.js";
 
 //app config
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 9000;
 
-const pusher = new Pusher({
- //Get from Pusher
-});
+const pusher = new Pusher({});
 
 //middleware
 app.use(express.json());
@@ -22,14 +20,11 @@ app.use((req, res, next) => {
 });
 
 //db config
-mongoose.connect(
-  //Get from MongoDB
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.once("open", () => {
   console.log("DB connected");
@@ -53,29 +48,11 @@ db.once("open", () => {
 });
 
 //api routes
-app.get("/", (req, res) => res.status(200).send("hello sanchit"));
+app.get("/", postRoutes);
 
-app.get("/messages/sync", (req, res) => {
-  Messages.find((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(data);
-    }
-  });
-});
+app.get("/messages/sync", postRoutes);
 
-app.post("/api/messages/new", (req, res) => {
-  const dbMessage = req.body;
-
-  Messages.create(dbMessage, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(data);
-    }
-  });
-});
+app.post("/api/messages/new", postRoutes);
 
 //listener
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
